045458b51a33eb3764f33da4ed380714292de443
npikall <nikolas.pikall@gmail.com>
2025-06-17 14:23:15 +0200

    [feat] added debugger

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,33 @@
+return {
+    {
+        "nvim-neotest/nvim-nio",
+    },
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "mfussenegger/nvim-dap-python",
+        },
+        config = function()
+            local dap, dapui = require("dap"), require("dapui")
+
+            require("dap-python").setup()
+            require("dapui").setup()
+
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
+            vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+            vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+        end,
+    },
+}
